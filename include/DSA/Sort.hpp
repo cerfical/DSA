8a772a03eabb07d5ba625e2913b431b76423dfb8
cerfical <138404639+cerfical@users.noreply.github.com>
2023-08-02 23:51:39 +0500

    Small changes

--- include/DSA/Sort.hpp
+++ include/DSA/Sort.hpp
@@ -49,15 +49,15 @@ template <std::ranges::random_access_range R>
 void insertionSort(R&& range) {
 	for(auto it = range.begin(); it != range.end(); it++) {
 		auto key = std::move(*it);
-		const auto insertKey = [&]() {
-			auto insertPos = it;
-			while(insertPos != range.begin() && *(insertPos - 1) > key) {
-				*insertPos = std::move(*(insertPos - 1));
-				insertPos--;
-			}
-			*insertPos = std::move(key);
-		};
-		insertKey();
+		auto insertPos = it;
+
+		// find the right place to insert the value
+		while(insertPos != range.begin() && *(insertPos - 1) > key) {
+			*insertPos = std::move(*(insertPos - 1));
+			insertPos--;
+		}
+
+		*insertPos = std::move(key);
 	}
 }
 
@@ -69,46 +69,42 @@ void insertionSort(R&& range) {
 template <std::ranges::random_access_range R>
 	requires std::sortable<std::ranges::iterator_t<R>>
 void mergeSort(R&& range) {
-	const auto merge = [&](auto left, auto right) {
-		// allocate temporary storage for the result
-		std::vector<std::ranges::range_value_t<decltype(left)>> result;
-		result.reserve(std::ranges::distance(left) + std::ranges::distance(right));
-
-		// iterate over the two ranges, saving the smallest element
-		while(!std::ranges::empty(left) && !std::ranges::empty(right)) {
-			if(left.front() < right.front()) {
-				result.emplace_back(std::move(left.front()));
-				left.advance(1);
-			} else {
-				result.emplace_back(std::move(right.front()));
-				right.advance(1);
-			}
-		}
-
-		// append remaining elements to the result
-		std::ranges::move(left, std::back_inserter(result));
-		std::ranges::move(right, std::back_inserter(result));
-
-		// move the sorted elements back to the original sequence
-		std::ranges::move(result, range.begin());
-	};
-
 	// if the range contains only one element or no elements at all, there is nothing to sort
 	if(std::ranges::size(range) <= 1) {
 		return;
 	}
 	
 	// find the midpoint of the range
-	const auto mid = range.begin() + std::ranges::distance(range) / 2;
+	const auto mid = range.begin() + std::ranges::size(range) / 2;
 
 	// split the range into two halves and sort them independently
-	const auto left = std::ranges::subrange(range.begin(), mid);
+	auto left = std::ranges::subrange(range.begin(), mid);
 	mergeSort(left);
 
-	const auto right = std::ranges::subrange(mid, range.end());
+	auto right = std::ranges::subrange(mid, range.end());
 	mergeSort(right);
 
-	merge(left, right);
+
+	// allocate temporary storage for the result
+	std::vector<std::ranges::range_value_t<R>> result;
+
+	// iterate over the two ranges, saving the smallest element
+	while(!std::ranges::empty(left) && !std::ranges::empty(right)) {
+		if(left.front() < right.front()) {
+			result.push_back(std::move(left.front()));
+			left.advance(1);
+		} else {
+			result.push_back(std::move(right.front()));
+			right.advance(1);
+		}
+	}
+
+	// append remaining elements to the result
+	std::ranges::move(left, std::back_inserter(result));
+	std::ranges::move(right, std::back_inserter(result));
+
+	// move the sorted elements back to the original sequence
+	std::ranges::move(result, range.begin());
 }
 
 
