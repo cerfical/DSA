76da3779ad142f7afb6355e744f83caa57acd625
cerfical <138404639+cerfical@users.noreply.github.com>
2023-12-08 02:11:48 +0500

    Refactor and simplift sorting algorithms implementation

--- include/DSA/Sort.hpp
+++ include/DSA/Sort.hpp
@@ -12,16 +12,20 @@ namespace dsa {
 	 * @brief Sorts a range of values in O(n^2) time.
 	 */
 	template <std::ranges::random_access_range R>
-		requires std::sortable<std::ranges::iterator_t<R>>
 	void bubbleSort(R&& range) {
-		bool sorted = false;
-		for(auto it1 = range.end(); it1 != range.begin() && !sorted; it1--) {
-			sorted = true;
+		for(auto it1 = range.end(); it1 != range.begin(); it1--) {
+			bool sorted = true;
+
 			for(auto it2 = range.begin(); it2 != it1 - 1; it2++) {
 				if(*(it2 + 1) < *it2) {
 					std::iter_swap(it2 + 1, it2);
 					sorted = false;
 				}
+			}
+
+			// exit early, if no element was swapped
+			if(sorted) {
+				break;
 			}
 		}
 	}
@@ -32,7 +36,6 @@ namespace dsa {
 	 * @brief Sorts a range of values in O(n^2) time.
 	 */
 	template <std::ranges::random_access_range R>
-		requires std::sortable<std::ranges::iterator_t<R>>
 	void selectionSort(R&& range) {
 		for(auto it = range.begin(); it != range.end(); it++) {
 			std::iter_swap(it,
@@ -47,7 +50,6 @@ namespace dsa {
 	 * @brief Sorts a range of values in O(n^2) time.
 	 */
 	template <std::ranges::random_access_range R>
-		requires std::sortable<std::ranges::iterator_t<R>>
 	void insertionSort(R&& range) {
 		for(auto it = range.begin(); it != range.end(); it++) {
 			auto key = std::move(*it);
@@ -69,28 +71,25 @@ namespace dsa {
 	 * @brief Sorts a range of values in O(n * lg n) time.
 	 */
 	template <std::ranges::random_access_range R>
-		requires std::sortable<std::ranges::iterator_t<R>>
 	void mergeSort(R&& range) {
-		// if the range contains only one element or no elements at all, there is nothing to sort
-		if(std::ranges::size(range) <= 1) {
+		const auto size = std::ranges::size(range);
+		if(size <= 1) {
 			return;
 		}
 
-		// find the midpoint of the range
-		const auto mid = range.begin() + std::ranges::size(range) / 2;
-
 		// split the range into two halves and sort them independently
+		const auto mid = range.begin() + size / 2;
 		auto left = std::ranges::subrange(range.begin(), mid);
-		mergeSort(left);
-
 		auto right = std::ranges::subrange(mid, range.end());
+		
+		mergeSort(left);
 		mergeSort(right);
-
-
-		// allocate temporary storage for the result
+		
+		// iteratively extract the smallest element from the two now sorted subsequences
+		// and put it in a temporary storage to build a complete sorted sequence
 		std::vector<std::ranges::range_value_t<R>> result;
+		result.reserve(size);
 
-		// iterate over the two ranges, saving the smallest element
 		while(!std::ranges::empty(left) && !std::ranges::empty(right)) {
 			if(left.front() < right.front()) {
 				result.push_back(std::move(left.front()));
@@ -101,55 +100,49 @@ namespace dsa {
 			}
 		}
 
-		// append remaining elements to the result
+		// collect leftovers from either the left or right, depending on which half was emptied first
 		std::ranges::move(left, std::back_inserter(result));
 		std::ranges::move(right, std::back_inserter(result));
 
-		// move the sorted elements back to the original sequence
 		std::ranges::move(result, range.begin());
 	}
 
 
 
 	/**
-	 * @brief Sorts a range of values in O(n * lg n) time.
+	 * @brief Sorts a range of values in O(n * lg n) time on average.
 	 */
 	template <std::ranges::random_access_range R>
-		requires std::sortable<std::ranges::iterator_t<R>>
 	void quickSort(R&& range) {
-		// if the range contains only one element or no elements at all, there is nothing to sort
-		if(std::ranges::size(range) <= 1) {
+		const auto size = std::ranges::size(range);
+		if(size < 2) {
 			return;
 		}
 
-		// temporary storage for values less than and greater that or equal to the pivot
-		std::vector<std::ranges::range_value_t<R>> less;
-		std::vector<std::ranges::range_value_t<R>> greater;
+		const auto pivotPos = range.end() - 1;
+		auto pivot = std::move(*pivotPos);
 
-		// find the pivot point for the range
-		const auto pivotIt = range.begin() + std::ranges::size(range) / 2;
-		auto pivot = std::move(*pivotIt);
+		auto lessPos = range.begin();
+		auto greaterPos = pivotPos;
 
-		for(auto it = range.begin(); it != range.end(); it++) {
-			// skip pivot point position
-			if(it == pivotIt) {
-				continue;
-			}
-
-			if(*it < pivot) {
-				less.push_back(std::move(*it));
+		// move elements less than the pivot to the beginning of the range, other elements are moved to the end
+		while(lessPos != greaterPos) {
+			if(*lessPos < pivot) {
+				lessPos++;
 			} else {
-				greater.push_back(std::move(*it));
+				std::iter_swap(lessPos, greaterPos - 1);
+				greaterPos--;
 			}
 		}
 
+		const auto less = std::ranges::subrange(range.begin(), lessPos);
+		const auto greater = std::ranges::subrange(lessPos, pivotPos);
+
 		quickSort(less);
 		quickSort(greater);
-
-		// move sorted subsequences into the original range
-		const auto outIt = std::ranges::move(less, range.begin()).out;
-		*outIt = std::move(pivot);
-		std::ranges::move(greater, outIt + 1);
+		
+		std::ranges::move_backward(greater, range.end());
+		*greaterPos = std::move(pivot);
 	}
 
 }
