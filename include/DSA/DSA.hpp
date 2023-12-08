ac6619739262134c8887f7da96cad8a15be0a776
cerfical <138404639+cerfical@users.noreply.github.com>
2023-12-09 04:24:23 +0500

    Stylistic changes

---
+++ include/DSA/DSA.hpp
@@ -0,0 +1,17 @@
+#ifndef DSA_ROOT_HEADER
+#define DSA_ROOT_HEADER
+
+#include "Search.hpp"
+#include "Sort.hpp"
+
+#include "List.hpp"
+#include "Heap.hpp"
+
+#include "HeapUtils.hpp"
+
+
+/** @brief The root namespace containing all available algorithm and data structure implementations. */
+namespace dsa { }
+
+
+#endif
