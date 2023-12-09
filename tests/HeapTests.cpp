27d4ee6e9067fc56801d94cfa490e2608531f5e5
cerfical <138404639+cerfical@users.noreply.github.com>
2023-12-09 22:52:25 +0500

    Simplify Heap's tests

--- tests/HeapTests.cpp
+++ tests/HeapTests.cpp
@@ -1,52 +1,30 @@
-#include <gtest/gtest.h>
+#include "AdapterTests.hpp"
 #include <DSA/Heap.hpp>
 
 
-class HeapTests : public testing::Test {
-protected:
-	
-	void pushAndCheck(int value, int expect) {
-		m_data.push(value);
-		ASSERT_EQ(m_data.top(), expect);
-	}
+using HeapTests = AdapterTests<dsa::Heap, int>;
 
-	void popAndCheck(int expect) {
-		ASSERT_EQ(m_data.pop(), expect);
-	}
-
-	void push(int value) {
-		m_data.push(value);
-	}
-
-private:
-	dsa::Heap<int> m_data;
-};
-
-
-TEST_F(HeapTests, pushPutsLargestValueOnTop) {
-	pushAndCheck(0, 0);
-	pushAndCheck(3, 3);
-	pushAndCheck(2, 3);
-	pushAndCheck(4, 4);
-	pushAndCheck(5, 5);
-	pushAndCheck(3, 5);
+TEST_F(HeapTests, pushPutsLargestElementInHeapTop) {
+	pushValueAndCheckTop(0, 0);
+	pushValueAndCheckTop(3, 3);
+	pushValueAndCheckTop(2, 3);
+	pushValueAndCheckTop(4, 4);
+	pushValueAndCheckTop(5, 5);
+	pushValueAndCheckTop(3, 5);
 }
 
-TEST_F(HeapTests, popExtractsElementsInDescendingOrder) {
-	push(4);
-	push(6);
-	popAndCheck(6);
+TEST_F(HeapTests, popReturnsLargestElement) {
+	pushValues(4, 6);
+	popAndCheckValue(6);
 
-	push(3);
-	push(3);
-	push(2);
-	push(5);
-	popAndCheck(5);
-	popAndCheck(4);
-	popAndCheck(3);
-	popAndCheck(3);
+	pushValues(3, 3, 2, 5);
+	popAndCheckValue(5);
+	popAndCheckValue(4);
+	popAndCheckValue(3);
+	popAndCheckValue(3);
 
-	push(6);
-	popAndCheck(6);
-	popAndCheck(2);
+	pushValues(6, 0);
+	popAndCheckValue(6);
+	popAndCheckValue(2);
+	popAndCheckValue(0);
 }
